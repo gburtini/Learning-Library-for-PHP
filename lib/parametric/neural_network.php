907ea25734a8a1aabea4aae6227f0600bc2d98f9
Giuseppe Burtini <joe@truephp.com>
2011-12-20 10:38:21 -0800

    A quick wrapper for what the neural network class interface should look like... still not certain how to best implement this.

--- lib/parametric/neural_network.php
+++ lib/parametric/neural_network.php
@@ -1,7 +1,23 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+   class LL_NeuralNetwork
+   {
+      var $inputNodes;
+      var $outputNodes;
+      var $hiddenNodes = array();
+
+      var $alpha = 0.3;
+      var $activationFunction = "ll_sigmoid";
+      /* Algorithm thanks to: Chris Marriott, Ryan Shirley, CJ Baker, Thomas Tannahill */
+
+      public function predict($inputNodes)
+      {
+         // iterates through the NN and computes an output array.
+      }
+
+      public function learn()
+      {
+         // trains the parameters in the array (via backprop?)
+      }
+   }
 ?>
