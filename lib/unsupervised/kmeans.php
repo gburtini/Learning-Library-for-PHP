8249e0d9d09a8a5783e4b286d029eaacf724cda9
Giuseppe Burtini <joe@truephp.com>
2011-12-21 10:32:14 -0800

    Added kMeans tests (except two, the most important two!), added comments to kMeans and fixed a bug in the initializer (detected by testing!)

--- lib/unsupervised/kmeans.php
+++ lib/unsupervised/kmeans.php
@@ -1,8 +1,30 @@
 <?php
+/* kmeans clustering
+ *
+ * kmeans clustering works in three discrete steps; our goal is to break data in to
+ * the k best possible ("most different"#) groups. the first step is to initialize the centroids to some
+ * random points (we initialize to random points that exist in the dataset, as this
+ * appears to be the best way to do it -- but you could change the _ll_init_centroids
+ * function to behave differently). the second step is to assign all data points to
+ * "belong" to a centroid, then the third step is to move the centroids to the average
+ * of all the data points that belong to it.
+ *
+ * the second and third steps are repeated until the centroids have stopped moving in
+ * our implementation.
+ *
+ * # while the goal is "most different", in fact there's the possibility of being
+ * trapped in local minima. the random nature of our initialization means that if you
+ * apply it multiple times, you may get different results. a score could be calculated
+ * for each result (based on sum of squared distances between $xs and centroids), but
+ * we have not yet provided a function to do that.
+ */
 
 // $xs is a two dimensional array (one dimension being observations, the inner dimension being integral "dimensions" of the observation)
 function ll_kmeans($xs, $k)
 {
+   if($k > count($xs))
+      return false;
+
    $centroids = _ll_init_centroids($xs, $k);
    $belongs_to = array();
    do {
@@ -23,6 +45,7 @@ function ll_kmeans($xs, $k)
    return $belongs_to;
 }
 
+// repositions the centroids to the average of all their member elements
 function _ll_reposition_centroids($centroids, $belongs_to, $xs)
 {
    for($index=0; $index<count($centroids); $index++)
@@ -47,6 +70,7 @@ function _ll_reposition_centroids($centroids, $belongs_to, $xs)
    return $centroids;
 }
 
+// makes rows in to columns; columns in to rows. "transposes" the matrix.
 function __ll_flip($rows)
 {
    $columns = array();
@@ -60,6 +84,7 @@ function __ll_flip($rows)
    return $columns;
 }
 
+// finds the closest centroid to a given $x, by Euclidian distance.
 function _ll_closest_centroid($x, $centroids)
 {
    $smallest = null;
@@ -76,6 +101,7 @@ function _ll_closest_centroid($x, $centroids)
    return $smallest;
 }
 
+// computes the Euclidian distance from a point x to a centroid $centroid.
 function __ll_distance_to_centroid($x, $centroid)
 {
    if(count($x) != count($centroid))
@@ -90,17 +116,21 @@ function __ll_distance_to_centroid($x, $centroid)
    return sqrt($distance);
 }
 
+// initializes the location of the centroids to a random data point.
 function _ll_init_centroids($xs, $k)
 {
+   if($k > count($xs))
+      return false;
+
    $centroids = array();
    for($i=0;$i<$k;$i++)
    {
-
       $temp_array = array();
-      $random = rand(0, count($xs)); // random row from data.
-      for($b=0;$b<count($xs[$random]);$b++) {
-         $temp_array = $xs[$random][$b];
-      }
+      $random = rand(0, count($xs)-1); // random row from data.
+      $temp_array = $xs[$random];
+      unset($xs[$random]); // don't allow the same centroid to be set twice.
+      $xs = array_values($xs);   // renumber the array
+
       $centroids[] = $temp_array;
    }
 
