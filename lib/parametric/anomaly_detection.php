07e59821a4fa4cde49db3a43fe04f1110da6103c
Giuseppe Burtini <joe@truephp.com>
2011-12-22 10:01:41 -0800

    Added anomaly detection tests and made it work properly.

--- lib/parametric/anomaly_detection.php
+++ lib/parametric/anomaly_detection.php
@@ -3,26 +3,30 @@
  * This could be easily modified to use any reasonable distribution, but as it is implemented
  * below, only supports the Gaussian (normal) distribution by changing computeProbabilities.
  * Recommended to subclass if you're going to do that.
+ *
+ * Thoughts: if you have an n-modal distribution (say bimodal), can you use k-means
+ * clustering to compute multiple anomaly detectors (one around each mode) and sum/f(x,y) their
+ * probabilities? maybe not because this would chop the edges of the distribution off.
  */
 require_once dirname(__FILE__) . "/../accessory/functions.php";
 
 class LL_AnomalyDetection {
    private $mean = array();
    private $variance = array();
-   private $oneSided;
+
+   private $learned = false;
 
    public function learn($xs)
    {
       $this->computeParameters($xs);
+      $this->learned = true;
    }
-
-   public function setOneSided($bool) {
-      $this->oneSided = $bool;
-   }
-   public function getOneSided() { return $this->oneSided; }
 
    public function isAnomaly($data_point, $p=0.01)
    {
+      if(!$this->learned)
+         return null;
+
       $probability = $this->computeProbability($data_point);
       if($p === false)
          return $probability;
@@ -33,7 +37,9 @@ class LL_AnomalyDetection {
    protected function computeProbability($x) {
       $prod = 1;
       foreach($x as $index=>$xi) {
-         $prod *= normalPdf($x, $this->mean[$index], $this->variance[$index]);
+
+         $prob = $this->normalPdf($xi, $this->mean[$index], $this->variance[$index]);
+         $prod *= $prob;
       }
       return $prod;
    }
@@ -43,7 +49,7 @@ class LL_AnomalyDetection {
    }
    private function computeParameters($xs)
    {
-      $xs_columns = ll_tranpose($xs);
+      $xs_columns = ll_transpose($xs);
       foreach($xs_columns as $index=>$column) {
          $this->mean[$index] = ll_mean($column);
          $this->variance[$index] = ll_variance($column);
