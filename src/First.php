9a2636c55bc431fa081a42b608086f9e7e9ab6ad
ReenExe <ipourri@gmail.com>
2016-01-19 11:29:06 +0200

    add `src`

---
+++ src/First.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Giuseppe\LearningLibrary;
+
+class First
+{
+
+}
--- tests/Test.php
+++ tests/Test.php
@@ -1,9 +1,11 @@
 <?php
 
+use Giuseppe\LearningLibrary\First;
+
 class Test extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
-
+        new First();
     }
 }
