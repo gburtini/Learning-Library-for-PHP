f4da3b4b54022be448c38ac1cb3cb606c863ea3b
ReenExe <ipourri@gmail.com>
2016-02-04 20:04:20 +0200

    move SANN tests

---
+++ tests/SannTest.php
@@ -0,0 +1,72 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+use Giuseppe\LearningLibrary\Parametric\Sann;
+
+class SannTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        function partition_objective($partitioning)
+        {
+            $sum1 = 0;
+            foreach ($partitioning[0] as $element)
+            {
+                $sum1 += $element;
+            }
+
+            $sum2 = 0;
+            foreach ($partitioning[1] as $element)
+            {
+                $sum2 += $element;
+            }
+
+            return abs($sum1 - $sum2);
+        }
+
+        function partition_neighbour($partitioning)
+        {
+            $which = mt_rand(0, 1);
+            if (count($partitioning[$which]) == 0)
+            {
+                $which = abs($which - 1);
+            }
+            $opposite = abs($which - 1);
+
+            if (count($partitioning[$which]) == 0)
+                return $partitioning;
+
+            // Move the element.
+            $toMove = mt_rand(0, count($partitioning[$which]) - 1);
+            $partitioning[$opposite][] = $partitioning[$which][$toMove];
+            unset($partitioning[$which][$toMove]);
+            $partitioning[$which]	   = array_values($partitioning[$which]);
+            $partitioning[$opposite]   = array_values($partitioning[$opposite]);
+            return $partitioning;
+        }
+
+        $initP = array(
+            array(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+            ),
+            array(
+            )
+        );
+
+        $instance = new Sann();
+
+        $objF = 'partition_objective';
+        $P = $instance->sann($initP,
+            $objF,
+            'partition_neighbour',
+            #'logarithmic_cooling',
+            #'linear_cooling',
+            #'exponential_cooling',
+            [$instance, 'unbounded_exponential_cooling'],
+            [$instance, 'default_accept'],
+            true);
+        $solutionScore = partition_objective($P);
+        echo "Objective of found solution: " . $solutionScore  . "\n";
+        var_dump($P);
+    }
+}
